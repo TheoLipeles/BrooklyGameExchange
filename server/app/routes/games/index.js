2b9a0b18853c6cfda2d999a90c898bc3c89c41c7
nickells <ellsworth.nick@gmail.com>
2015-07-27 16:25:07 -0400

    fixed game routes

--- server/app/routes/games/index.js
+++ server/app/routes/games/index.js
@@ -6,22 +6,9 @@ var Game = require('../../../db/models/game.js')
 var Review = require('../../../db/models/review.js')
 
 router.get('/', function (req,res){
-<<<<<<< HEAD
-	Game.find({})
-	.then(function(games){
-		res.send(games.data)
-=======
-<<<<<<< Updated upstream
-	console.log("hey!")
 	Game.find({})
 	.then(function(games){
 		res.send(games)
-=======
-	Game.find({})
-	.then(function(games){
-		res.send(games.data)
->>>>>>> Stashed changes
->>>>>>> seed
 	},function(){
 		res.sendStatus(404)
 	})
@@ -30,13 +17,8 @@ router.get('/', function (req,res){
 router.get('/:id', function (req,res){
 	Game.find({_id: req.params.id})
 	.then(function(game){
-<<<<<<< Updated upstream
 		res.send(game)
 	},function(err){
-=======
-		res.send(game.data)
-	},function(){
->>>>>>> Stashed changes
 		res.sendStatus(404)
 	})
 })
@@ -44,13 +26,8 @@ router.get('/:id', function (req,res){
 router.get('/:id/reviews', function(req,res){
 	Review.find({game: req.params.id})
 	.then(function(reviews){
-<<<<<<< Updated upstream
 		res.send(reviews)
 	},function(err){
-=======
-		res.send(reviews.data)
-	},function(){
->>>>>>> Stashed changes
 		res.sendStatus(404)
 	})
 })
