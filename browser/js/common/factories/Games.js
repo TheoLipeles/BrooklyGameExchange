9b90741d3789eecebdf938b6846882ae71e148f3
nickells <ellsworth.nick@gmail.com>
2015-07-28 17:53:01 -0400

    fixed up browser page, added sorting, added css, modularized things

--- browser/js/common/factories/Games.js
+++ browser/js/common/factories/Games.js
@@ -11,8 +11,7 @@ app.factory('Games', function ($http) {
 		getOne: function(id){
 			return $http.get('/api/games/'+id)
 			.then(function(game){
-				//I AM MAKING THIS [0] FOR NOW BUT WHEN IT BREAKS, GO LOOK AT THE ROUTES 
-				return game.data[0]
+				return game.data
 			});
 		}
 
