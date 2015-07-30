aec6a2cfbf928e9b178452028654b241d10c1595
Isaac Ratner <isaacratner@gmail.com>
2015-07-30 18:57:12 -0400

    working shopping cart!

--- browser/js/cart/cart.js
+++ browser/js/cart/cart.js
@@ -1,20 +1,29 @@
 app.config(function ($stateProvider) {
 
 	$stateProvider.state('cart', {
-		url: 'user/:id/cart',
-		templateUrl: 'js/cart/cart.html',
+		url: '/user/:id/cart',
+		templateUrl: '/js/cart/cart.html',
 		controller: 'CartCtrl'
 	});
 
 });
 
-app.controller('CartCtrl', function ($scope, $stateParams){
+app.controller('CartCtrl', function ($scope, $stateParams, User){
+	
+	User.getOne($stateParams.id)
+	.then(function(user) {
+		$scope.name = user.name;
+		$scope.cart = user.cart;
+	});
 
-	// Users.getOne($stateParams.id)
-	// .then(function(user) {
-	// 	$scope.name = user.name;
-	// 	$scope.cart = user.cart;
-	// })
+	$scope.getTotal = function() {
+		console.log("$scope.cart", $scope.cart);
+		var total = 0;
+		for(var i = 0; i < $scope.cart.length; i++) {
+			total+=$scope.cart[i].price;
+		}
+		return total;
+	}
 
 
 
