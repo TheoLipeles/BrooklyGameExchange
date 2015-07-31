b2995a9a3e9bc736bee0b2dc75924b7ad10c1134
celanajaya <petermsteele@gmail.com>
2015-07-31 15:41:14 -0400

    login and signup

--- server/env/development.js
+++ server/env/development.js
@@ -4,16 +4,16 @@ module.exports = {
   "TWITTER": {
     "consumerKey": "JhNXWbRVUbDJRySp0hUNZ0E0p",
     "consumerSecret": "plcZIT0V9qlGSoVJZ6o2vXQrkbDgVo56tLCEGlbViMfXwTYdZf",
-    "callbackUrl": "http://localhost:1337/user/:id"
+    "callbackUrl": "http://localhost:1337/users"
   },
   "FACEBOOK": {
     "clientID": "876077149140383",
     "clientSecret": "c5292bec7ea9f446bff567bfb49d6ff4",
-    "callbackURL": "http://localhost:1337/user/:id"
+    "callbackURL": "http://localhost:1337/users"
   },
   "GOOGLE": {
     "clientID": "273851523537-qgkpi362880p246in8cless9e19glg76.apps.googleusercontent.com",
     "clientSecret": "iaCgC5sHqNSX9OaUQwXlAhvO",
-    "callbackURL": "http://localhost:1337/user/:id"
+    "callbackURL": "http://localhost:1337/users"
   }
 };
