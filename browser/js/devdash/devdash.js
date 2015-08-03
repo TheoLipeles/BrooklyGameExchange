29ac98929f12df83cf69d77747bdd073ebf8ca7c
celanajaya <petermsteele@gmail.com>
2015-08-03 14:53:43 -0400

    added devdash stuff

--- browser/js/devdash/devdash.js
+++ browser/js/devdash/devdash.js
@@ -6,41 +6,46 @@ app.config(function ($stateProvider) {
         controller: 'DevDashCtrl',
         resolve: {
         	user: function(AuthService){return AuthService.getLoggedInUser()}
-        }
-    });
+    	}
+	});
 
 });
 
 app.controller('DevDashCtrl',function ($scope, User, user, AuthService){
 	$scope.user = user;
-	$scope.newGame = {}
+	$scope.newGame = {};
 	var updateUser = function() {
 		User.getOne(user._id).then(function(dev) {
-			console.log(dev.createdGames)
+			console.log(dev.createdGames);
 			$scope.user = dev;
 		});
+	};
+
+	$scope.totalDownloads = function() {
+		return user.createdGames.reduce(function(prev, game){
+			return prev + game.downloads
+		}, 0);
 	}
+
 	updateUser();
 
-	// var Game = function(title, description, genre, screenshots, minPrice){
-	// 	this.title = title;
-	// 	this.description = description;
-	// 	this.genre = genre;
-	// 	this.screenshots = screenshots;
-	// 	this.minPrice = minPrice;
-	// 	this.developer = user._id
-	// }
+    $scope.reset = function() {
+    	$scope.newGame = {
+        	title: null,
+        	description: null,
+        	genre: null,
+        	screenshot: false,
+        	minPrice: null,
+        	downloadLink: null
+    	};
+	};
 
 	$scope.postGame = function(){
-		// console.log("hi")
-		// var thisGame = new Game(newGame)
-		// $scope.newGame.developer = user._id
 		User.postGame(user._id, $scope.newGame)
 		.then(function() {
-			// $scope.user = user;
-
+			updateUser();
+			$scope.reset();
 		});
-		updateUser();
-		// $scope.user.createdGames.unshift(thisGame)
-	}
+	};
+
 });
