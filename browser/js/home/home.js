78e52ea96d6965191339e87aef43c6f892c8424f
nickells <ellsworth.nick@gmail.com>
2015-08-04 21:26:00 -0400

    the little things that no one notices

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -2,7 +2,14 @@ app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
         templateUrl: 'js/home/home.html',
-        controller: function($rootScope){
+        resolve: {
+        	user: function(AuthService){
+        		return AuthService.getLoggedInUser()
+        	}
+        },
+        controller: function($rootScope,user,$scope){
+        	$scope.user = user;
+        	console.log(user)
         	$rootScope.showNavBar = true;
         }
     });
