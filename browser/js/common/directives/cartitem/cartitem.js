5e263db85cabed5b69f0102e741e5ac7ae60856e
Ubuntu <ubuntu@ip-172-31-44-109.us-west-2.compute.internal>
2015-08-05 13:51:10 +0000

    fixed cartitem

---
+++ browser/js/common/directives/cartitem/cartitem.js
@@ -0,0 +1,10 @@
+app.directive('cartItem', function() {
+	return {
+		restrict: 'E',
+		templateUrl: 'js/common/directives/cartitem/cartitem.html'
+	};
+});
+
+// app.controller('cartItemCtrl', function($scope,User) {
+// 	console.log($scope)
+// });
